1b1b8210173eefee9c5d316d67b5625f7cf6c3af
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-05-21 17:57:03 +0530

    Update meter.py

--- tkdial/meter.py
+++ tkdial/meter.py
@@ -63,7 +63,7 @@ class Meter(tk.Canvas):
 
         if not self.bg:
             try:
-                if master.winfo_name()=="!ctkframe":
+                if master.winfo_name().startswith("!ctkframe"):
                     # get bg_color of customtkinter frames
                     self.bg = master._apply_appearance_mode(master.cget("fg_color"))
                 else:
